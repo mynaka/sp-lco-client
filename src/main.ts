2d863956b5feb9519f99f95989218f3ac9ae10c4
mynaka <avillanueva@up.edu.ph>
2024-07-04 03:31:14 +0800

    feat: home search page

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
-import './style.css'
+import './index.css'
 import App from './App.vue'
+import router from './router';
+import PrimeVue from 'primevue/config';
+import Select from 'primevue/select';
 
-createApp(App).mount('#app')
+const app = createApp(App).use(router);
+app.use(PrimeVue);
+app.component('Select', Select);
+app.mount('#app');
---
+++ src/pages/Home.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="min-h-screen flex items-center justify-center">
+    <div class="bg-gray-100 p-8 rounded-lg shadow-lg flex flex-col justify-center items-center">
+    <form class="flex flex-col md:flex-row gap-3">
+      <div class="flex">
+        <input type="text" placeholder="Search Terms" v-model="searchTerm"
+        class="w-full md:w-80 px-3 h-10 rounded-l border-2 border-red-700 focus:outline-none focus:border-red-700"
+        >
+          <button @click="search" type="button" class="bg-red-700 text-white rounded-r px-2 md:px-3 py-0 md:py-1">Search</button>
+      </div>
+      <MultiSelect 
+        v-model="selectedOntology" 
+        :options="ontology_options" 
+        filter 
+        placeholder="Select Ontologies"
+        :maxSelectedLabels="3" class="w-full md:w-80" />
+  </form>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import MultiSelect from 'primevue/multiselect';
+  import { ref } from 'vue';
+
+  const selectedOntology = ref([]);
+  const ontology_options = ref([
+    'DOID'
+  ]);
+
+  const searchTerm = ref();
+
+  function search() {
+    console.log(selectedOntology);
+    console.log(searchTerm);
+  }
+</script>
+
+<style scoped>
+</style>
+  
